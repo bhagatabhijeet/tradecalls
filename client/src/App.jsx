40c045ed1634211ee0d876cb79180d2ef432b52e
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2021-01-14 12:58:25 -0800

    Add React BootStrap as dependency

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import HomePage from "./pages/HomePage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
     <Router>
-      <Route exact path="/" component="HomePage.jsx"/>
+      <Route exact path="/" component={HomePage}/>
       <Route path="/signin" component={SignIn}/>
       <Route path="/signup" component={SignUp}/>
     </Router>
